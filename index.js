1dfa828e240594e33809601a9f7d804eb84a1d06
xaviguasch <xaviguasch@gmail.com>
2020-08-09 19:52:50 +0200

    added fetch catch exercise and solved it

--- index.js
+++ index.js
@@ -34,10 +34,25 @@
 
 // Fetch an image from https://jsonplaceholder.typicode.com/photos/1. The response will be in a JSON. You'll need to access the "url" property, then pass that into the img tag.
 
-fetch('https://jsonplaceholder.typicode.com/photos/1') // Add the URL
-  .then((res) => res.json()) // Process the data
-  .then((json) => {
-    const img = document.createElement('img')
-    img.src = json.url // Set the image source to the image URL value
-    document.body.appendChild(img)
+// fetch('https://jsonplaceholder.typicode.com/photos/1') // Add the URL
+//   .then((res) => res.json()) // Process the data
+//   .then((json) => {
+//     const img = document.createElement('img')
+//     img.src = json.url // Set the image source to the image URL value
+//     document.body.appendChild(img)
+//   })
+
+// Add error handling to this example
+
+fetch('people.json')
+  .then((response) => {
+    throw 404
   })
+  .then((json) =>
+    json.forEach((person) => {
+      const div = document.createElement('div')
+      div.innerHTML = `${person.name} is ${person.age}`
+      document.body.appendChild(div)
+    })
+  )
+  .catch((err) => console.error(err))
