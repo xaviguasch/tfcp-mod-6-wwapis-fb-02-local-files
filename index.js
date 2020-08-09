02e7670149b97e743cfe8123dffc821fc1977aeb
xaviguasch <xaviguasch@gmail.com>
2020-08-09 19:37:29 +0200

    added fetch a photo from an api exercise

--- index.js
+++ index.js
@@ -22,12 +22,23 @@
 // EXERCISE 2
 // Fetch the data from the .json file and display each persons name and age like this: "John is 20".
 
-fetch('./people.json')
-  .then((res) => res.json())
-  .then((json) => {
-    json.forEach((person) => {
-      const div = document.createElement('div')
-      div.innerHTML = `${person.name} is ${person.age}.`
-      document.body.appendChild(div)
-    })
-  })
+// fetch('./people.json')
+//   .then((res) => res.json())
+//   .then((json) => {
+//     json.forEach((person) => {
+//       const div = document.createElement('div')
+//       div.innerHTML = `${person.name} is ${person.age}.`
+//       document.body.appendChild(div)
+//     })
+//   })
+
+
+// Fetch an image from https://jsonplaceholder.typicode.com/photos/1. The response will be in a JSON. You'll need to access the "url" property, then pass that into the img tag.
+
+fetch() // Add the URL
+	.then(res => res) // Process the data
+	.then(json => { 
+		const img = document.createElement('img');
+		img.src = ; // Set the image source to the image URL value
+		document.body.appendChild(img);
+	}); 
