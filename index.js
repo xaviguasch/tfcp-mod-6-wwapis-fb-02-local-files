5a8a55cb389d8e980d3a21b1cfe168426509fe72
xaviguasch <xaviguasch@gmail.com>
2020-08-09 18:08:31 +0200

    added solution

--- index.js
+++ index.js
@@ -8,3 +8,13 @@
 
 // EXERCISE
 // Fetch the data from the .json file and display each persons name on the web page.
+
+fetch('./people.json')
+  .then((res) => res.json())
+  .then((json) => {
+    json.forEach((person) => {
+      const div = document.createElement('div')
+      div.innerHTML = person.name
+      document.body.appendChild(div)
+    })
+  })
