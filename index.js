2be0b12b88ba05e296a28a9fbbf9cbb2bffafbf4
xaviguasch <xaviguasch@gmail.com>
2020-08-09 17:53:44 +0200

    added new fetch exercise using json data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-fetch('./example.txt')
-  .then((res) => res.text())
-  .then((res) => {
-    const div = document.createElement('div')
-    div.innerHTML = res
-    document.body.append(div)
-  })
+// fetch('./example.txt')
+//   .then((res) => res.text())
+//   .then((res) => {
+//     const div = document.createElement('div')
+//     div.innerHTML = res
+//     document.body.append(div)
+//   })
+
+// EXERCISE
+// Fetch the data from the .json file and display each persons name on the web page.
