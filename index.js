baa9cde3d51f0432d31954573529f179d812df5b
xaviguasch <xaviguasch@gmail.com>
2020-08-09 18:21:34 +0200

    added and solved 2nd exercise

--- index.js
+++ index.js
@@ -9,12 +9,25 @@
 // EXERCISE
 // Fetch the data from the .json file and display each persons name on the web page.
 
+// fetch('./people.json')
+//   .then((res) => res.json())
+//   .then((json) => {
+//     json.forEach((person) => {
+//       const div = document.createElement('div')
+//       div.innerHTML = person.name
+//       document.body.appendChild(div)
+//     })
+//   })
+
+// EXERCISE 2
+// Fetch the data from the .json file and display each persons name and age like this: "John is 20".
+
 fetch('./people.json')
   .then((res) => res.json())
   .then((json) => {
     json.forEach((person) => {
       const div = document.createElement('div')
-      div.innerHTML = person.name
+      div.innerHTML = `${person.name} is ${person.age}.`
       document.body.appendChild(div)
     })
   })
