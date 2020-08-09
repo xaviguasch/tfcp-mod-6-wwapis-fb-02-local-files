c5f2a4d0ea41255e2ea87b9eea2b5cd7c3da4456
xaviguasch <xaviguasch@gmail.com>
2020-08-09 19:43:04 +0200

    added solution

--- index.js
+++ index.js
@@ -32,13 +32,12 @@
 //     })
 //   })
 
-
 // Fetch an image from https://jsonplaceholder.typicode.com/photos/1. The response will be in a JSON. You'll need to access the "url" property, then pass that into the img tag.
 
-fetch() // Add the URL
-	.then(res => res) // Process the data
-	.then(json => { 
-		const img = document.createElement('img');
-		img.src = ; // Set the image source to the image URL value
-		document.body.appendChild(img);
-	}); 
+fetch('https://jsonplaceholder.typicode.com/photos/1') // Add the URL
+  .then((res) => res.json()) // Process the data
+  .then((json) => {
+    const img = document.createElement('img')
+    img.src = json.url // Set the image source to the image URL value
+    document.body.appendChild(img)
+  })
